4dba26228b8f9843e1db16155a9662fe620cb889
Anderson-Wes <anderson.wes420@gmail.com>
2025-03-07 19:33:40 +0400

    seach, movie not found

--- movie-finder/src/app/features/search/search.component.ts
+++ movie-finder/src/app/features/search/search.component.ts
@@ -1,10 +1,10 @@
-import { Component, signal, effect } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { MovieService } from '../../core/services/movie.service';
 import { IMovie } from '../../shared/interfaces/interfaces';
-import { MovieCardComponent } from '../../shared/movie-card/movie-card.component';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { MovieCardComponent } from '../../shared/movie-card/movie-card.component';
 
 @Component({
   standalone: true,
@@ -14,26 +14,52 @@ import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
-  private movieService = new MovieService(); //create an instance of `MovieService`
+  private readonly movieService = inject(MovieService);
 
-  searchControl = new FormControl('');
-  movies = signal<{ Search: IMovie[] } | null>(null); //stores a list of found movies
-  movies$ = this.searchControl.valueChanges.pipe(
-    distinctUntilChanged(), //the request is sent only if the text has changed
-    debounceTime(500),
-    switchMap((query) => this.movieService.searchMovies(query ?? '')) //Cancels the previous request if the user continues typing
-  );
+  searchControl = new FormControl(''); // Input field for search
+  public movies = signal<IMovie[]>([]); // Stores search results
+  public isSearching = signal<boolean>(false); // Indicates if a search is in progress
+  public hasSearched = signal<boolean>(false); // Indicates if a search has been performed
 
   constructor() {
-    effect(() => {
-      this.searchControl.valueChanges.pipe().subscribe((query) => {
-        //tracks changes in the input field
-        if (query) {
-          this.movieService
-            .searchMovies(query)
-            .subscribe((data) => this.movies.set(data)); //updating the list of found films
-        }
+    // Listen for input changes and perform search automatically
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(500), // Wait 500ms before sending request
+        distinctUntilChanged(), // Ignore duplicate queries
+        switchMap((query) => {
+          if (!query?.trim()) {
+            this.movies.set([]); // Clear results if input is empty
+            this.hasSearched.set(false); // Reset search state
+            return [];
+          }
+          this.isSearching.set(true);
+          this.hasSearched.set(true); // Mark that a search has been performed
+          return this.movieService.searchMovies(query);
+        })
+      )
+      .subscribe((data) => {
+        this.movies.set(data?.Search ?? []);
+        this.isSearching.set(false);
       });
+  }
+
+  /**
+   * Manually triggers search when "Search" button is clicked.
+   */
+  public performSearch(): void {
+    const query = this.searchControl.value?.trim();
+    if (!query) {
+      this.movies.set([]);
+      this.hasSearched.set(false); // Reset search state
+      return;
+    }
+
+    this.isSearching.set(true);
+    this.hasSearched.set(true); // Mark that a search has been performed
+    this.movieService.searchMovies(query).subscribe((data) => {
+      this.movies.set(data?.Search ?? []);
+      this.isSearching.set(false);
     });
   }
 }
